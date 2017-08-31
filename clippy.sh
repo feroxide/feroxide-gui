5f671bc4dc79c0a764ab5ee1a3c98bb8b0f46f4f
Cxarli <10348289+Cxarli@users.noreply.github.com>
2017-08-31 22:07:44 +0200

    Fix Travis failing on installing Clippy when it's already in cache

--- clippy.sh
+++ clippy.sh
@@ -4,6 +4,12 @@ mode="$1"
 
 if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
   if [ "$mode" = "install" ]; then
+    # Test if clippy is already installed (cache)
+    $CARGO clippy &> /dev/null
+    if [ $? -ne 101 ]; then
+      exit 0
+    fi
+
     cargo install clippy
     exit $?
   fi
