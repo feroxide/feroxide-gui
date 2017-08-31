a0f6d788f13221dfaaec6c0969bd45603632f874
Cxarli <10348289+Cxarli@users.noreply.github.com>
2017-08-31 21:29:19 +0200

    Fix no_utf feroxide

--- clippy.sh
+++ clippy.sh
@@ -5,8 +5,13 @@ mode="$1"
 if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
   if [ "$mode" = "install" ]; then
     # Test if clippy is already installed (cache)
+<<<<<<< HEAD
     if cargo clippy -V; then
       echo "Clippy already installed, skipping installation"
+=======
+    cargo clippy &> /dev/null
+    if [ $? -ne 101 ]; then
+>>>>>>> 97a3db4 (Fix Travis failing on installing Clippy when it's already in cache)
       exit 0
     fi
 
